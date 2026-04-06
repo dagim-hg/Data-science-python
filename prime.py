c3042b24e9200d55dcdf872e83ae010451dc7458
Dagim HG <251992901+dagim-hg@users.noreply.github.com>
2026-04-06 11:18:05 +0300

    Initial commit

---
+++ prime.py
@@ -0,0 +1,16 @@
+# function that check if the number is prime
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+num = int(input())
+
+if is_prime(num):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
